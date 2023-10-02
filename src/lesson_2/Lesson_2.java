b9310ef0640661e51e9e0281b21c67bda9def48d
vladimir <vladimir197717@mail.ru>
2023-10-02 08:28:42 +0400

    lessons and tasks 1-5

---
+++ src/lesson_2/Lesson_2.java
@@ -0,0 +1,8 @@
+package lesson_2;
+
+public class Lesson_2 {
+    public static void main(String[] args) {
+        String value = "In JavaRush, Diego the best, Diego is Java God".replace("Diego", "Amigo");
+        System.out.println(value);
+    }
+}
---
+++ src/lesson_3/Lesson_3.java
@@ -0,0 +1,8 @@
+package lesson_3;
+
+public class Lesson_3 {
+    public static void main(String[] args) {
+
+
+    }
+}
